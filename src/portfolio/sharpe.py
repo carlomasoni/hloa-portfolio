f27af6b20a1ea0129c6be6325eee6c124b9a6126
carlomasoni <carlo.masoni@outlook.com>
2025-09-19 14:22:03 +0100

    skeleton for optimisation
    -> check how metaheuristics are relevant in certain dimenssions, or if at all necessary for max-sharpe?
    -> is pypoft actually a good library? -> check for better alternatives
    -> is higher n optimisation more suited for HLOA.

--- src/portfolio/sharpe.py
+++ src/portfolio/sharpe.py
@@ -1,4 +1,37 @@
+from base64 import standard_b64decode
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import datetime as dt
+import scipy as sp
+from datetime import datetime, timedelta  
+import pypoft      
+
+tickers = [ 'STOXX50E' ] # QQQ, VTI + other markets when needed
+end = datetime.now()
+start = end - timedelta(days=30)
+
+
+adjusted_closed_dfs = pd.DataFrame([])
+for ticker in tickers:
+    adjusted_closed_dfs[ticker] = yf.download(ticker, start=start, end=end)['Adj Close']
+
+
+# Returns
+
+log_nreturns = np.log(adjusted_closed_dfs) / adjusted_closed_dfs.shift(1) 
+log_nreturns = log_nreturns.dropna() 
+
+
+cov_matrix = log_nreturns.cov()
+
+
+
+
+
+
+
+
+
+
 
